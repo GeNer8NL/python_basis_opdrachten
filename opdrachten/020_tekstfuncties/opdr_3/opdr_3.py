43d6c3f471efa2eb6816a1f80c17838ab6eea4c7
GeNer8NL <97060864@st.deltion.nl>
2023-11-29 10:25:40 +0100

    Versie 2

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -4,20 +4,11 @@
 
 # Hier komt je code...
 
-def mijn_boom():
-    print("    *\n   ***\n  ******\n ********\n***********\n    ***\n    ***\n    ***"),
-
-mijn_boom()
-print(mijn_boom())
-
-
-print("    *")
-print("   ***")
-print("  ******")
-print(" ********")
-print("***********")
-print("    ***")
-print("    ***")
-print("    ***")
-
-* Work in progress *
+print("    *                " * 5)
+print("   ***               " * 5)
+print("  ******             " * 5)
+print(" ********            " * 5)
+print("***********          " * 5)
+print("    ***              " * 5)
+print("    ***              " * 5)
+print("    ***              " * 5)
