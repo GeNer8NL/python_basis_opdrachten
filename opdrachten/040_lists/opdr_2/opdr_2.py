51bb3247ce987b984a3b6cf08aef485fb8b6f0b2
GeNer8NL <97060864@st.deltion.nl>
2023-11-30 15:06:59 +0100

    Versie 1

--- opdrachten/040_lists/opdr_2/opdr_2.py
+++ opdrachten/040_lists/opdr_2/opdr_2.py
@@ -3,13 +3,14 @@
 # Groep:
 
 
-rivier_info = {
+rivier_info ={
     "rijn": ["nederland", "duitsland", "Frankrijk"],
     "maas": ["nederland", "belgië", "duitsland"],
-    "nijl": ["egypte", "soedan", "oeganda"]
-}
-
+    "nijl": ["egypte", "soedan", "oeganda"]}
 rivieren = list(rivier_info.keys())
-# rivieren is nu een list met alleen de riviernamen: ['rijn', 'maas', 'nijl']
 
-# Hier jouw code.....
+print("De rivier", rivieren [0].capitalize(), "stroomt onder andere door", rivier_info ["rijn"][1].capitalize())
+
+print("De rivier", rivieren [1].capitalize(), "stroomt onder andere door", rivier_info ["maas"][0].capitalize())
+
+print("De rivier", rivieren [2].capitalize(), "stroomt onder andere door", rivier_info ["nijl"][2].capitalize())
