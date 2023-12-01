cd95127cb96301e7c4099042bdf95c49b95b9f7c
GeNer8NL <97060864@st.deltion.nl>
2023-12-01 11:10:42 +0100

    Versie 1

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -3,4 +3,23 @@
 # Groep:
 
 # Hier komt je code...
+def get_input():
+    print("We willen nu graag uw 5 favoriete steden weten")
+    stad1 = input("Noem een stad naar keuze: ")
+    stad2 = input("Noem nog een stad naar keuze: ")
+    stad3 = input("Noem nog een stad naar keuze: ")
+    stad4 = input("Noem nog een stad naar keuze: ")
+    stad5 = input("Noem een laatste stad naar keuze: ")
+    steden= sorted([stad1,stad2,stad3,stad4,stad5], reverse=True)
+    print("zijn dit uw steden?",steden )
+get_input()
+def Eind():
+    confirm = input("Type Y als u wilt doorgaan, Typ N als u terug wilt gaan")
+    while True:
+        if confirm == "Y":
+            print("Veel plezier")
+            break
+        else:
+            return get_input(), Eind()
 
+Eind()
