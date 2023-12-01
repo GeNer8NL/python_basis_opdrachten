0de287aecdb1a81dc0d3aae3da7d4e819b2ffe94
GeNer8NL <97060864@st.deltion.nl>
2023-12-01 09:24:20 +0100

    Versie 2

--- opdrachten/040_lists/opdr_2/opdr_2.py
+++ opdrachten/040_lists/opdr_2/opdr_2.py
@@ -9,8 +9,8 @@ rivier_info ={
     "nijl": ["egypte", "soedan", "oeganda"]}
 rivieren = list(rivier_info.keys())
 
-print("De rivier", rivieren [0].capitalize(), "stroomt onder andere door", rivier_info ["rijn"][1].capitalize())
+print("De rivier de", rivieren [0].capitalize(), "stroomt onder andere door", rivier_info ["rijn"][1].capitalize())
 
-print("De rivier", rivieren [1].capitalize(), "stroomt onder andere door", rivier_info ["maas"][0].capitalize())
+print("De rivier de", rivieren [1].capitalize(), "stroomt onder andere door", rivier_info ["maas"][0].capitalize())
 
-print("De rivier", rivieren [2].capitalize(), "stroomt onder andere door", rivier_info ["nijl"][2].capitalize())
+print("De rivier de", rivieren [2].capitalize(), "stroomt onder andere door", rivier_info ["nijl"][2].capitalize())
--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -2,6 +2,13 @@
 # Naam student:
 # Groep:
 
+
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
+import math
 
-
+Zijde1 = float(input("Geef de lengte van de eerste zijde: "))
+Zijde2 = float(input("Geef de lengte van de tweede zijde: "))
+eind = Zijde1 ** 2 + Zijde2 ** 2
+antwoord = math.sqrt(eind)
+afgerond = round(antwoord, 2)
+print("De lengte van de schuine zijde is", afgerond)
